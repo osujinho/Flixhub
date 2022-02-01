b9e9c0679ebda8982cef8705aa1f06c21abdec81
Michael Osuji <michaelchenko@gmail.com>
2022-02-01 11:49:27 -0500

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Michael Osuji on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
